e814a80d0db8c6a54cea3f18a1680d9531804e89
ahaoboy <504595380@qq.com>
2025-10-05 18:34:56 +0800

    ci: ext

--- download.sh
+++ download.sh
@@ -2,14 +2,21 @@ DOWNLOAD_URL="$1"
 TARGET="$2"
 
 FILE_NAME=$(basename "$DOWNLOAD_URL")
-EXT="${FILE_NAME##*.}"
+
+if [[ "$FILE_NAME" == *.* ]]; then
+  EXT=".${FILE_NAME##*.}"
+else
+  EXT=""
+fi
+
+DIST="dist-${TARGET}"
 
 curl -L -o "${FILE_NAME}" "$DOWNLOAD_URL"
 
 chmod +x "${FILE_NAME}"
 
-mkdir -p dist
+mkdir -p $DIST
 
-cp "${FILE_NAME}" "dist/kiesel${EXT}"
+cp "${FILE_NAME}" "$DIST/kiesel${EXT}"
 
-tar -czf ./kiesel-${TARGET}.tar.gz -C dist .
+tar -czf ./kiesel-${TARGET}.tar.gz -C $DIST .
