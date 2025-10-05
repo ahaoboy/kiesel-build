e658b971d4c75dea0e4c94ebba6f30d856995e66
ahaoboy <504595380@qq.com>
2025-10-05 18:27:50 +0800

    ci: download

---
+++ download.sh
@@ -0,0 +1,13 @@
+DOWNLOAD_URL="$1"
+TARGET="$2"
+
+FILE_NAME=$(basename "$DOWNLOAD_URL")
+EXT="${FILE_NAME##*.}"
+
+curl -L -o "${FILE_NAME}" "$DOWNLOAD_URL"
+
+mkdir -p dist
+
+cp "${FILE_NAME}" "dist/kiesel${EXT}"
+
+tar -czf ./kiesel-${TARGET}.tar.gz -C dist .
